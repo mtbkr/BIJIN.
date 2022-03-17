575714691b1100cdc2183c369e2d80fc6c6bd9ab
Harumu Matsumoto <pdpdpd4248@gmail.com>
2022-03-17 19:40:01 +0900

    Add: Bootstrap修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,5 @@
 import 'bootstrap';
-import '../stylesheets/application';
-
+import '../stylesheets/application.scss';
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -14,3 +13,7 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
